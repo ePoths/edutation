1806257c5e34cfbde2db9f2fd678a1e8a8f200e0
김주엽 <gimjuyeob@gimjuyeob-ui-MacBookAir.local>
2023-11-02 21:07:56 +0900

    modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import Dummy from "./Dommy";
 
 const Container = styled.div`
   background-color: ${(prop) => prop.theme.bgColor};
@@ -10,31 +9,9 @@ const H1 = styled.h1`
 `;
 
 function App() {
-  const [value, setValue] = useState("");
-  const OnChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(`Succes! : Value(${value})`);
-  };
   return (
     <Container>
       <H1>Hello, Words!</H1>
-      <form action="" onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={OnChange}
-          type="text"
-          placeholder="UserName"
-        />
-        <button>Log in</button>
-      </form>
-
-      <Dummy text={"hello"}></Dummy>
     </Container>
   );
 }
--- src/Dommy.tsx
+++
@@ -1,9 +0,0 @@
-interface DummyTyps {
-  text: string;
-  active?: boolean;
-}
-
-function Dummy({ text, active = false }: DummyTyps) {
-  return <div>{text}</div>;
-}
-export default Dummy;
